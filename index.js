ba6a6f2fb3ab87e1fadce1132a84e7375d53f8d2
digitalishous <digitalishous@gmail.com>
2026-03-19 12:50:16 -0500

    html, css, js

---
+++ index.js
@@ -0,0 +1,47 @@
+
+//the audio wouldn't play first, before the .intro page loaded, no matter what I tried. I even tried with a promise, so that only once the audio started playing the rest of the page would load, but nothing. I tried with standard delays 100-600 as well, also nothing. Finally, the only thing that worked was a delay of 2500 (have it at 3000 now). I believe it's the browser's behaviour with the sound and autoplay that creates a whole mess of things. Should be addressed!
+addEventListener("click", () => {
+    const audio = document.getElementById("audioFile");
+    const body = document.getElementById("intro");
+    const crawl = document.querySelector(".crawl");
+
+    audio.currentTime = 0;
+    audio.play();
+
+    setTimeout(() => {
+        body.classList.add("show");
+        crawl.style.animationPlayState = "running";
+    }, 3000);
+
+}, { once: true }); // this is needed so that the listener only runs once, and then shots down
+
+
+
+    // let playPromise = audio.play();
+    //
+    // if (playPromise !== undefined) {
+    //     playPromise.then(() => {
+    //
+    //
+    //
+    //         setTimeout(() => {
+    //             body.classList.add("show");
+    //             crawl.style.animationPlayState = "running";
+    //         }, 300); //THIS SMALL DELAY WAS THE ISSUE ALL LONG. 
+    //     })
+    // }
+
+
+
+    // audio.play().then(() => {
+    //     document.querySelector(".crawl").style.animationPlayState = "running";
+    //     document.getElementById("intro").classList.add("show");
+    // })
+// },
+// {once: true}
+//
+// );
+
+
+
+
